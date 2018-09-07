a9b6bb2c381daca167b4d495e7e9983926cd91ef
cryingjoker <cryingjoker@me.com>
2018-09-07 13:59:48 +0300

    deploy

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp');
+const shell = require('gulp-shell');
+
+gulp.task('copy', shell.task([
+        'rm -rf ./docs',
+        'mkdir docs',
+        'git merge master -m\'deploy\'\n',
+        'npm run build',
+        'cp -R ./dist/* ./docs',
+        'cd ./docs',
+        'ls',
+        'perl -pi -w -e \'s/src=\\//src=/g\' ./docs/index.html',
+        'perl -pi -w -e \'s/"\\/"/""/\' ./docs/main.js',
+        'perl -pi -w -e \'s/\\/example-images\\//\\/vue-stylekit\\/example-images\\//g\' ./docs/main.js\n',
+        'perl -pi -w -e \'s/\\/images\\//\\/vue-stylekit\\/images\\//g\' ./docs/main.js\n',
+        'git commit -a -m\'deploy\'\n',
+        'git push'
+    ])
+)
+
+
+
+
