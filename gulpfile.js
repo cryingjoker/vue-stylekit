2f521192c84ca4b42263524f6874f77bfadec97c
cryingjoker <cryingjoker@me.com>
2018-09-12 13:14:08 +0300

    deploy

--- gulpfile.js
+++ gulpfile.js
@@ -15,9 +15,9 @@ gulp.task('copy', shell.task([
         'perl -pi -w -e \'s/\\/example-images\\//\\/vue-stylekit\\/example-images\\//g\' ./docs/main.js\n',
         'perl -pi -w -e \'s/\\/images\\//\\/vue-stylekit\\/images\\//g\' ./docs/main.js\n',
         'rm -rf ./dist',
-        'git commit -a -m\'deploy\'\n',
-        'git push',
-        'git checkout master'
+        // 'git commit -a -m\'deploy\'\n',
+        // 'git push',
+        // 'git checkout master'
     ])
 )
 //.@{newAppName} -> rt
