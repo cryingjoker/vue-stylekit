a0619ef4334a12bf6a23aadb7e3b74eb7271976b
cryingjoker <cryingjoker@me.com>
2018-09-13 08:32:03 +0300

    change gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -19,10 +19,11 @@ gulp.task(
     "perl -pi -w -e 's/path:\\\"\\//path:\\\"\\/vue\\-stylekit\\/docs\\//g' ./docs/main.js\n",
     "perl -pi -w -e 's/to\\:\\\"\\//to\\:\\\"\\/vue\\-stylekit\\/docs\\//g' ./docs/main.js\n",
     "perl -pi -w -e 's/src=\\//src=\\/vue\\-stylekit\\/docs\\//g' ./docs/index.html",
-    "rm -rf ./dist",
-    "git commit -a -m'deploy'\n",
-    "git push",
-    "git checkout master"
+    "perl -pi -w -e 's/BasisGrotesqu/docs\\/BasisGrotesqu/g' ./docs/main.js",
+    // "rm -rf ./dist",
+    // "git commit -a -m'deploy'\n",
+    // "git push",
+    // "git checkout master"
   ])
 )
 //.@{newAppName} -> rt
