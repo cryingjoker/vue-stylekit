1e152870735bb15fc299f1eeeb5c73cf6122dee3
cryingjoker <cryingjoker@me.com>
2018-09-19 14:51:14 +0300

    fix

--- src/components/test-components/pre-component-editor.vue
+++ src/components/test-components/pre-component-editor.vue
@@ -5,6 +5,23 @@
 
 <script>
   import * as monaco from 'monaco-editor';
+  // window.MonacoEnvironment = {
+  //   getWorkerUrl: function (moduleId, label) {
+  //     if (label === 'json') {
+  //       return './json.worker.bundle.js';
+  //     }
+  //     if (label === 'css') {
+  //       return './css.worker.bundle.js';
+  //     }
+  //     if (label === 'html') {
+  //       return './html.worker.bundle.js';
+  //     }
+  //     if (label === 'typescript' || label === 'javascript') {
+  //       return './ts.worker.bundle.js';
+  //     }
+  //     return './editor.worker.bundle.js';
+  //   }
+  // }
   export default {
     name: "PreCodeEditor",
     props: {
