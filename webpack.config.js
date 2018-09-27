dc9dca808451266c855baf131fc8773435da2364
cryingjoker <cryingjoker@me.com>
2018-09-27 23:45:04 +0300

    move font-face to standalone file

--- webpack.config.js
+++ webpack.config.js
@@ -56,7 +56,10 @@ const config = {
       },
       {
         test: /\.css$/,
-        use: ['css-loader'],
+        use: [
+            // MiniCssExtractPlugin.loader,
+      {loader:`css-loader`},
+        ],
       },
       {
         test: /\.js$/,
@@ -69,9 +72,8 @@ const config = {
           {
             loader: `style-loader`,
           },
-          {
-            loader: `css-loader`,
-          },
+          // MiniCssExtractPlugin.loader,
+        {loader:`css-loader`},
           {
             loader: `less-loader`,
           },
@@ -83,9 +85,8 @@ const config = {
           {
             loader: `style-loader`,
           },
-          {
-            loader: `css-loader`,
-          },
+          // MiniCssExtractPlugin.loader,
+        {loader:`css-loader`},
           {
             loader: `stylus-loader`,
           },
