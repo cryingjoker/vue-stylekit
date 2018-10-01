314ebcf17ad2cf31014363dce98b969bdda09db4
cryingjoker <cryingjoker@me.com>
2018-10-01 09:04:52 +0300

    add dynamic triangle for baners

--- src/app/Content/experimental-elements/Banner/Banner.vue
+++ src/app/Content/experimental-elements/Banner/Banner.vue
@@ -21,11 +21,11 @@
       </div>
     </div>
     <div class="rt-banner-image" :style="imageStyle">
-      <svg v-if="!isFullscreenImage" class="rt-banner-triangle" xmlns="http://www.w3.org/2000/svg">
-        <path d="M0 0v500h2L185 0z" fill-rule="evenodd" />
+      <svg v-if="!isFullscreenImage" class="rt-banner-triangle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 185 500" transform='translate(-50% 0)'>
+        <polygon points="0 0,0 500,185 0 "/>
       </svg>
-      <svg v-if="!isFullscreenImage" class="rt-banner-right-triangle" xmlns="http://www.w3.org/2000/svg">
-        <path d="M0 0v500h2L185 0z" fill-rule="evenodd" />
+      <svg v-if="!isFullscreenImage" class="rt-banner-right-triangle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 185 500">
+        <polygon points="0 500,185 0,0 0"/>
       </svg>
     </div>
 
