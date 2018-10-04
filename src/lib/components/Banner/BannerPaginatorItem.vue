bc37f8a761988a75c3401749fe5769d606cfd0ca
cryingjoker <cryingjoker@me.com>
2018-10-04 16:04:03 +0300

    fix banner + add logo option

--- src/lib/components/Banner/BannerPaginatorItem.vue
+++ src/lib/components/Banner/BannerPaginatorItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="circle-switcher__item" :class="paginatorItemClass" @click="setActiveItem">
+  <div class="circle-switcher__item" :class="paginatorItemClass" :style="paginatorItemStyle" @click="setActiveItem">
     <svg xmlns="http://www.w3.org/2000/svg" class="circle-switcher__icon" :class="paginatorItemIconClass" viewBox="0 0 9 9">
       <circle r="4" cx="4.5" cy="4.5" fill="transparent" />
     </svg>
@@ -11,6 +11,10 @@ export default {
   name: 'RtBannerPaginatorItem',
   props: {
     index: {
+      type: Number,
+      default: null
+    },
+    sleepTime:{
       type: Number,
       default: null
     }
@@ -32,6 +36,13 @@ export default {
         classArray['rt-banner--run-timer'] = true;
       }
       return classArray;
+    },
+    paginatorItemStyle(){
+      if(this.sleepTime){
+        return {
+          animationDuration: this.sleepTime/1000 +'s'
+        }
+      }
     },
     paginatorItemIconClass() {
       const classArray = {};
