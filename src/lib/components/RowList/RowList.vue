a439ee6bfbdf13d4ad37761cfc860496bb9d01ea
cryingjoker <cryingjoker@me.com>
2018-10-17 11:31:47 +0300

    fix row-list and fix table

--- src/lib/components/RowList/RowList.vue
+++ src/lib/components/RowList/RowList.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="rt-row-list">
+  <div class="rt-row-list row">
     <slot/>
   </div>
 </template>
--- src/lib/components/RowList/RowListItem.vue
+++ src/lib/components/RowList/RowListItem.vue
@@ -2,47 +2,50 @@
   export default {
     name: "RtRowListItem",
     data: () => ({
-      show: false,
+      show: false
     }),
     methods: {
-      toggleShow(){
-        console.info('toggleShow')
+      toggleShow() {
+        console.info("toggleShow");
         this.show = !this.show;
       }
 
     },
     render: function(h) {
       if (this.$slots.moreInfo) {
-        return <div class={"rt-row-list__item rt-row-list__item--with-slide"+(this.show ? " rt-row-list__item--active" : "")} onClick={this.toggleShow}>
-          <div class="rt-row-list__header">
-            <div class="rt-row-list__option">
-              {this.$slots.icon ? <div class="rt-row-list__icon">
-                {this.$slots.icon}
-              </div> : null}
-              <div class="rt-row-list__content">
-                {this.$slots.option}
-              </div>
+        return <div
+          class={"rt-row-list__item rt-col-12 rt-col-md-3" + (this.show ? " rt-row-list__item--active" : "")}
+          onClick={this.toggleShow}>
+          <div class="rt-row-list__header row">
+            {this.$slots.icon ? <div class="rt-row-list__icon rt-col-1 rt-col-md-3 rt-md-space-bottom">
+              {this.$slots.icon}
+            </div> : null}
+            <div class={this.$slots.icon ? "rt-row-list__content rt-col-7 rt-col-md-3" : "rt-row-list__content rt-col-8 rt-col-md-3"}>
+              {this.$slots.option}
             </div>
-            <div class="rt-row-list__value">
+            <div class="rt-row-list__value rt-col-4 rt-col-md-3 rt-md-space-top">
               {this.$slots.value}
             </div>
           </div>
-          <div class={"rt-row-list__body"+(this.$slots.icon ? " rt-row-list__body--has-icon-padding" : "")}>
-            {this.$slots.moreInfo}
+          <div class={"rt-row-list__body row"}>
+              {this.$slots.icon ? <div class="rt-col-1 rt-col-md-3"></div> : null}
+              <div class={this.$slots.icon ? "rt-col-7 rt-col-md-3" : "rt-col-8 rt-col-md-3"}>
+                {this.$slots.moreInfo}
+              </div>
           </div>
         </div>;
       } else {
-        return <div class="rt-row-list__item">
-          <div class="rt-row-list__option">
-            {this.$slots.icon ? <div class="rt-row-list__icon">
+        return <div class="rt-row-list__item rt-col-12 rt-col-md-3">
+          <div class="rt-row-list__header row">
+            {this.$slots.icon ? <div class="rt-row-list__icon rt-col-1 rt-col-md-3 rt-md-space-bottom">
               {this.$slots.icon}
             </div> : null}
-            <div class="rt-row-list__content">
+            <div class={this.$slots.icon ? "rt-row-list__content rt-col-7 rt-col-md-3" : "rt-row-list__content rt-col-8 rt-col-md-3"}>
               {this.$slots.option}
             </div>
-          </div>
-          <div class="rt-row-list__value">
-            {this.$slots.value}
+            <div class="rt-row-list__value rt-col-4 rt-col-md-3 rt-md-space-top">
+              {this.$slots.value}
+            </div>
           </div>
         </div>;
       }
