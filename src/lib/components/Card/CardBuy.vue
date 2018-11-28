ca17820fea506173d52ab222f34e6288016d74f5
cryingjoker <cryingjoker@me.com>
2018-11-28 15:38:12 +0300

    new long card for listing

--- src/lib/components/Card/CardBuy.vue
+++ src/lib/components/Card/CardBuy.vue
@@ -9,6 +9,18 @@ export default {
   components: componentsList,
   directives: directives,
   props: {
+    isHorizontalCard:{
+      type: Boolean,
+      default: false
+    },
+    hasCustomButton: {
+      type: Boolean,
+      default: false
+    },
+    customButtonClass: {
+      type: String,
+      default: null
+    }
   },
   data(){
     return {
@@ -17,9 +29,40 @@ export default {
   },
   computed: {},
 
-  mounted: function() {
+  mounted() {
+    if(this.hasCustomButton && this.customButtonClass){
+      this.bindCustomButton()
+    }
+  },
+  beforeDestroy(){
+    if(this.hasCustomButton && this.customButtonClass) {
+      this.unbindCustomButton()
+    }
+  },
+  updated(){
+    if(this.hasCustomButton && this.customButtonClass) {
+      this.unbindCustomButton()
+      this.bindCustomButton()
+    }
   },
   methods: {
+    customButtonClick(){
+
+         if(this.showForm){
+           if(this.submitForm){
+             this.submitForm()
+           }
+         }else{
+           this.toggleShow()
+         }
+    },
+    bindCustomButton(){
+      this.$el.querySelector(`.${this.customButtonClass}`).addEventListener('click',this.customButtonClick);
+    },
+    unbindCustomButton(){
+      this.$el.querySelector(`.${this.customButtonClass}`).removeEventListener('click',this.customButtonClick);
+
+    },
     hideShow(){
       if(this.showForm){
         this.showForm = false;
@@ -42,17 +85,21 @@ export default {
 
   render(h) {
     const button = ()=>{
-      if(this.showForm){
-        if(this.submitForm) {
-          return <rt-button onClick={this.submitForm}
+      if(!this.hasCustomButton || !this.customButtonClass) {
+        if (this.showForm) {
+          if (this.submitForm) {
+            return <rt-button onClick={this.submitForm}
+                              class="rt-button--is-block rt-button-orange">{this.$slots.button}</rt-button>;
+          }
+          else {
+            return <rt-button class="rt-button--is-block rt-button-orange">{this.$slots.button}</rt-button>;
+          }
+        } else {
+          return <rt-button onClick={this.toggleShow}
                             class="rt-button--is-block rt-button-orange">{this.$slots.button}</rt-button>;
         }
-        else{
-          return <rt-button class="rt-button--is-block rt-button-orange">{this.$slots.button}</rt-button>;
-        }
       }else{
-        return <rt-button onClick={this.toggleShow}
-                          class="rt-button--is-block rt-button-orange">{this.$slots.button}</rt-button>;
+        return null
       }
     };
     const contentWithoutWrapper = (()=>{
@@ -60,11 +107,24 @@ export default {
         return this.$slots['content-without-wrapper']
       }
     })();
+    const bottomContent = ()=>{
+      if(!this.hasCustomButton || !this.customButtonClass) {
+        return <div className="rt-card__border-content rt-space-bottom">
+          {this.$slots['card-footer']}
+          {button()}
+        </div>
+      }else{
+        if(this.$slots['card-footer']){
+          return <div class="d-static">{this.$slots['card-footer']}</div>
+        }else{
+          return null
+        }
+      }
+    }
     return <div ref="card" v-rt-out-side-click={this.hideShow}>
-      <div class={'rt-card rt-card-buy'+(this.showForm ? ' rt-card-buy--show-form' : '')}>
-
+      <div class={'rt-card rt-card-buy'+(this.showForm ? ' rt-card-buy--show-form' : '') + (this.isHorizontalCard ? ' rt-card-buy--is-horizontal' : '')}>
         <div class="row rt-card-buy__info">
-          <div class="rt-card-buy__form rt-space-vertical">
+          <div class={"rt-card-buy__form rt-space-vertical"+(this.isHorizontalCard ? " rt-card-buy__form-horizontal":"")}>
             <div class="rt-close" onClick={this.hideShow}>
               <svg class="rt-close__icon" width="14" height="14" xmlns="http://www.w3.org/2000/svg">
                 <path d="M14 1.4L12.6 0 7 5.6 1.4 0 0 1.4 5.6 7 0 12.6 1.4 14 7 8.4l5.6 5.6 1.4-1.4L8.4 7z"
@@ -79,10 +139,7 @@ export default {
           </div>
         </div>
         {contentWithoutWrapper}
-        <div class="rt-card__border-content rt-space-bottom">
-          {this.$slots['card-footer']}
-          {button()}
-        </div>
+        {bottomContent()}
       </div>
     </div>;
   }
