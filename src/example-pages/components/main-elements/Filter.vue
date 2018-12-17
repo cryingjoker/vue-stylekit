c74b9edc8e10e6bd2efa3b129bf05917adf38341
cryingjoker <cryingjoker@me.com>
2018-12-17 14:13:42 +0300

    fix filter close button

--- src/example-pages/components/main-elements/Filter.vue
+++ src/example-pages/components/main-elements/Filter.vue
@@ -1693,7 +1693,7 @@
             <div class="rt-col-1 td-d-none">
             </div>
             <rt-popup popup-wrapper-classes="rt-col-3" popup-classes="rt-col-td-3 rt-col-md-3" name="filter">
-              <div class="rt-close">
+              <div class="rt-close d-none td-d-block">
                 <svg class="rt-close__icon" width="14" height="14" xmlns="http://www.w3.org/2000/svg">
                   <path d="M14 1.4L12.6 0 7 5.6 1.4 0 0 1.4 5.6 7 0 12.6 1.4 14 7 8.4l5.6 5.6 1.4-1.4L8.4 7z"
                         fill-rule="evenodd"/>
