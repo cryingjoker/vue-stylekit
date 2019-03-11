d02de23183c46e4f4eaad4b47682128565cbb501
cryingjoker <cryingjoker@me.com>
2019-03-11 12:52:19 +0300

    fix

--- src/example-pages/components/promo/test-access.vue
+++ src/example-pages/components/promo/test-access.vue
@@ -1,65 +1,47 @@
 <template>
   <div class="rt-space-top35">
-    <pre-code text="
-    <div class='"test-access"'
-    >
-      <div class="test-access__image mobile-none" />
-    </pre-code>
-  </div>
-  <div class="rt-container rt-col-12 rt-col-td-3 rt-col-md-3 rt-space-top25 d-flex rt-space-bottom25">
-    <div class="rt-col-5 rt-col-td-3 rt-col-md-3 flex-column">
-      <h3 class="rt-space-bottom rt-font-bold">
-        Тестовый доступ
-      </h3>
-      <div class="rt-col-10 rt-col-td-6 rt-col-md-3">
-        <p class="rt-font-paragraph rt-space-bottom">
-          Бесплатный доступ ко всем возможностям сервиса на 7 дней:
-        </p>
-      </div>
-      <ul class="rt-numeric-list rt-space-bottom">
-        <li class="rt-ordered-list__item rt-font-paragraph">
-          Гостевой доступ к личному кабинету с возможностью полного управления, чтобы
-          изучить его удобство  и функциональность
-        </li>
-        <li class="rt-ordered-list__item rt-font-paragraph">
-          Неограниченную входящую связь на выбранный городской номер
-        </li>
-        <li class="rt-ordered-list__item rt-font-paragraph">
-          50 минут исходящей связи
-        </li>
-        <li class="rt-ordered-list__item rt-font-paragraph">
-          Оформить коммерческий тариф и желаемые дополнительные услуги необходимо до
-          конца тестового периода
-        </li>
-      </ul>
-      <div class="stretcher">
-        <rt-button class="rt-button rt-button-orange">
-          Попробовать
-        </rt-button>
-      </div>
-      <a href="#">
-        <div class="download-button--dashed">
-          <span class="rt-font-small-paragraph text--underlined">Руководство пользователя для тестового периода</span>
-          <p class="rt-font-control">pdf, 1,3 Мб</p>
+    <pre-code
+      text="<div class=&quot;test-access&quot;>
+    <div class=&quot;test-access__image mobile-none&quot;></div>
+    <div class=&quot;rt-container rt-col-12 rt-col-td-3 rt-col-md-3 rt-space-top25 d-flex rt-space-bottom25&quot;>
+      <div class=&quot;rt-col-5 rt-col-td-3 rt-col-md-3 flex-column&quot;>
+        <h3 class=&quot;rt-space-bottom rt-font-bold&quot;>Тестовый доступ</h3>
+        <div class=&quot;rt-col-10 rt-col-td-6 rt-col-md-3&quot;>
+          <p class=&quot;rt-font-paragraph rt-space-bottom&quot;>Бесплатный доступ ко всем возможностям сервиса на 7 дней:</p>
         </div>
-      </a>
+        <ul class=&quot;rt-numeric-list rt-space-bottom&quot;>
+          <li class=&quot;rt-ordered-list__item rt-font-paragraph&quot;>Гостевой доступ к личному кабинету с возможностью полного управления, чтобы
+            изучить его удобство  и функциональность</li>
+          <li class=&quot;rt-ordered-list__item rt-font-paragraph&quot;>Неограниченную входящую связь на выбранный городской номер</li>
+          <li class=&quot;rt-ordered-list__item rt-font-paragraph&quot;>50 минут исходящей связи</li>
+          <li class=&quot;rt-ordered-list__item rt-font-paragraph&quot;>Оформить коммерческий тариф и желаемые дополнительные услуги необходимо до
+            конца тестового периода</li>
+        </ul>
+        <div class=&quot;stretcher&quot;>
+          <rt-button class=&quot;rt-button rt-button-orange&quot;>Попробовать</rt-button>
+        </div>
+        <a href=&quot;#&quot;>
+          <div class=&quot;download-button--dashed&quot;>
+            <span class=&quot;rt-font-small-paragraph text--underlined&quot;>Руководство пользователя для тестового периода</span>
+            <p class=&quot;rt-font-control&quot;>pdf, 1,3 Мб</p>
+          </div>
+        </a>
+      </div>
+      <div class=&quot;rt-col-1 flex-column mobile-none&quot;></div>
+      <div class=&quot;rt-space-top05 flex-column mobile-none&quot;></div>
     </div>
-    <div class="rt-col-1 flex-column mobile-none" />
-    <div class="rt-space-top05 flex-column mobile-none" />
-  </div>
-  </div>
-  />
+  </div>"/>
   </div>
 </template>
 
 <script>
-const componentsList = {};
+  const componentsList = {};
 
-export default {
-  name: "AppTestAccess",
-  components: componentsList,
-  mounted() {},
-  created() {},
-  methods: {}
-};
+  export default {
+    name: "AppTestAccess",
+    components: componentsList,
+    mounted() {},
+    created() {},
+    methods: {}
+  };
 </script>
