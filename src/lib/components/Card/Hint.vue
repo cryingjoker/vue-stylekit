41d0df6a7239793010ac2fbb41417526478ed8b2
wikked_one <wikked616@gmail.com>
2019-03-13 18:16:21 +0300

    b2b input hint add-on + row-list add-on

--- src/lib/components/Card/Hint.vue
+++ src/lib/components/Card/Hint.vue
@@ -1,41 +1,89 @@
-<template>
-  <div class="rtb-card__hint">
-    <div
-      class="rtb-card__hint__icon"
-      @mouseover="this.showHint"
-      @mouseleave="this.hideHint"
-    >
-      ?
-    </div>
-    <div v-show="active" class="rtb-card__hint__text">
-      <slot />
-    </div>
-  </div>
-</template>
-
-<script>
-export default {
-  name: "Hint",
-  data() {
-    return {
-      active: false
-    };
-  },
-  computed: {},
-  mounted() {},
-  created() {},
-
-  methods: {
-    showHint() {
-      let hint = this.$el;
-      hint.closest(".higher-z").style.zIndex = "2";
-      this.active = true;
+<script type="text/jsx">
+  export default {
+    name: "RtHint",
+    props: {
+      simpleToolTip: {
+        type: Boolean,
+        default: false
+      },
+      confirmation: {
+        type: Boolean,
+        default: false
+      },
+      confirmationPassed: {
+        type: Boolean,
+        default: false
+      }
     },
-    hideHint() {
-      let hint = this.$el;
-      hint.closest(".higher-z").style.zIndex = "1";
-      this.active = false;
+    data() {
+      return {
+        active: false
+      };
+    },
+    computed: {
+      hintClass() {
+        let className = 'rtb-hint';
+        if(this.simpleToolTip) {
+          className += ' rtb-hint--tooltip'
+        }
+        if(this.confirmation) {
+          if(this.confirmationPassed) {
+            className += ' rtb-hint--confirmed'
+          } else {
+            className += ' rtb-hint--denied'
+          }
+        }
+        return className;
+      }
+    },
+    mounted() {},
+    created() {},
+
+    methods: {
+      showHint(){
+        let hint = this.$el;
+        if(hint.closest(".higher-z"))
+          hint.closest(".higher-z").style.zIndex = '2';
+        this.active = true;
+      },
+      hideHint(){
+        let hint = this.$el;
+        if(hint.closest(".higher-z"))
+          hint.closest(".higher-z").style.zIndex = '1';
+        this.active = false;
+      }
+    },
+    render(h) {
+      const toolTipType = () => {
+        if(this.simpleToolTip) {
+          return <span>?</span>
+        }
+        if(this.confirmation) {
+          if(this.confirmationPassed) {
+            return <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg">
+              <g fill="none" fill-rule="evenodd">
+                <circle fill="#57D9B7" cx="10" cy="10" r="10"/>
+                <path stroke="#FFF" stroke-width="2" d="M5 10l3.793 3L16 7"/>
+              </g>
+            </svg>
+          } else {
+            return <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg">
+              <g fill="none" fill-rule="evenodd">
+                <circle fill="#FF0C0C" cx="10" cy="10" r="10"/>
+                <path d="M10 10.207l-4.51 4.51 4.51-4.51 4.51 4.51-4.51-4.51zm0 0L14.207 6 10 10.207 5.793 6 10 10.207z"
+                      stroke="#FFF" stroke-width="2"/>
+              </g>
+            </svg>
+          }
+        }
+      };
+
+      return <div class={this.hintClass}>
+        <div class="rtb-hint__icon" onMouseover={this.showHint} onMouseleave={this.hideHint}>{toolTipType()}</div>
+        <div v-show={this.active} class="rtb-hint__text">
+          {this.$slots.default}
+        </div>
+      </div>;
     }
-  }
-};
+  };
 </script>
