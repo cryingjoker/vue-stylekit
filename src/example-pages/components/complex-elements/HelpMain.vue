a58f6770f41331e82ce7db7bd1d86847a5708f55
wikked_one <wikked616@gmail.com>
2019-03-26 16:08:37 +0300

    banner scroll-bar fix + help main page

---
+++ src/example-pages/components/complex-elements/HelpMain.vue
@@ -0,0 +1,143 @@
+<template>
+  <div class="app-content rt-col-12 rt-col-md-3">
+    <div class="rt-container">
+      <div class="row">
+        <div class="rt-col-12 rt-col-td-6 rt-col-md-3">
+          <div class="app-title ">
+            <h1 class="rt-font-hero">Help Main Page</h1>
+          </div>
+        </div>
+
+          <div class="rt-col-12 rt-col-td-6 rt-col-md-3">
+            <div class="rt-space-bottom2">
+              <rt-row-list :is-help-block="true" class="rt-font-left">
+                <rt-row-list-item icon="/static/images/B2B/ATS.svg">
+                  <template slot="option">
+                    <a href="#"><p class="rt-font-paragraph rt-font-bold rt-space-bottom05">Виртуальная АТС</p></a>
+                    <a href="#"><p class="rt-font-small-paragraph rt-space-bottom05">Настройка переадресации вызовов</p>
+                    </a>
+                    <a href="#"><p class="rt-font-small-paragraph rt-space-bottom05">Как перевести звонок другому
+                      абоненту</p></a>
+                    <a href="#"><p class="rt-font-small-paragraph">Как настроить короткие номера</p></a>
+                  </template>
+                </rt-row-list-item>
+                <rt-row-list-item icon="/static/images/B2B/help2.svg">
+                  <template slot="option">
+                    <a href="#"><p class="rt-font-paragraph rt-font-bold rt-space-bottom05">8 800</p></a>
+                    <a href="#"><p class="rt-font-small-paragraph rt-space-bottom05">В Ростелеком со своим номером</p>
+                    </a>
+                    <a href="#"><p class="rt-font-small-paragraph rt-space-bottom05">Как проверить баланс в
+                      роуминге?</p></a>
+                    <a href="#"><p class="rt-font-small-paragraph">Интернет в роуминге</p></a>
+                  </template>
+                </rt-row-list-item>
+                <rt-row-list-item icon="/static/images/B2B/help3.svg">
+                  <template slot="option">
+                    <a href="#"><p class="rt-font-paragraph rt-font-bold rt-space-bottom05">Видеонаблюдение</p></a>
+                    <a href="#"><p class="rt-font-small-paragraph rt-space-bottom05">Как перейти в Ростелеком со своим
+                      номером?</p></a>
+                    <a href="#"><p class="rt-font-small-paragraph rt-space-bottom05">Как проверить баланс в
+                      роуминге?</p></a>
+                    <a href="#"><p class="rt-font-small-paragraph">Как подключить интернет в роуминге?</p></a>
+                  </template>
+                </rt-row-list-item>
+                <rt-row-list-item icon="/static/images/B2B/mobile-call.svg">
+                  <template slot="option">
+                    <a href="#"><p class="rt-font-paragraph rt-font-bold rt-space-bottom05">Виртуальная АТС</p></a>
+                    <a href="#"><p class="rt-font-small-paragraph rt-space-bottom05">Настройка переадресации вызовов</p>
+                    </a>
+                    <a href="#"><p class="rt-font-small-paragraph rt-space-bottom05">Как перевести звонок другому
+                      абоненту</p></a>
+                    <a href="#"><p class="rt-font-small-paragraph">Как настроить короткие номера</p></a>
+                  </template>
+                </rt-row-list-item>
+                <rt-row-list-item icon="/static/images/B2B/help5.svg">
+                  <template slot="option">
+                    <a href="#"><p class="rt-font-paragraph rt-font-bold rt-space-bottom05">8 800</p></a>
+                    <a href="#"><p class="rt-font-small-paragraph rt-space-bottom05">В Ростелеком со своим номером</p>
+                    </a>
+                    <a href="#"><p class="rt-font-small-paragraph rt-space-bottom05">Как проверить баланс в
+                      роуминге?</p></a>
+                    <a href="#"><p class="rt-font-small-paragraph">Интернет в роуминге</p></a>
+                  </template>
+                </rt-row-list-item>
+                <rt-row-list-item icon="/static/images/B2B/help6.svg">
+                  <template slot="option">
+                    <a href="#"><p class="rt-font-paragraph rt-font-bold rt-space-bottom05">Видеонаблюдение</p></a>
+                    <a href="#"><p class="rt-font-small-paragraph rt-space-bottom05">Как перейти в Ростелеком со своим
+                      номером?</p></a>
+                    <a href="#"><p class="rt-font-small-paragraph rt-space-bottom05">Как проверить баланс в
+                      роуминге?</p></a>
+                    <a href="#"><p class="rt-font-small-paragraph">Как подключить интернет в роуминге?</p></a>
+                  </template>
+                </rt-row-list-item>
+              </rt-row-list>
+            </div>
+
+            <h3 class="rt-font-bold rt-space-bottom2">Все сервисы</h3>
+            <div class="row rt-space-bottom5">
+              <div class="rt-col-4 rt-col-td-3 rt-col-md-3">
+                <a href=""><p class="rt-font-small-paragraph rt-font-bold rt-space-bottom05">Он-лайн касса</p></a>
+                <a href=""><p class="rt-font-small-paragraph rt-font-bold rt-space-bottom05">M2M</p></a>
+                <a href=""><p class="rt-font-small-paragraph rt-font-bold rt-space-bottom05">СМС-реклама</p></a>
+                <a href=""><p class="rt-font-small-paragraph rt-font-bold rt-space-bottom05">Телевидение</p></a>
+                <a href=""><p class="rt-font-small-paragraph rt-font-bold rt-space-bottom05">Аренда сетевого
+                  оборудования</p></a>
+                <a href=""><p class="rt-font-small-paragraph rt-font-bold rt-space-bottom05">Защита от DDoS-атак</p></a>
+              </div>
+              <div class="rt-col-4 rt-col-td-3 rt-col-md-3">
+                <a href=""><p class="rt-font-small-paragraph rt-font-bold rt-space-bottom05">Аренда сетевого
+                  оборудования</p></a>
+                <a href=""><p class="rt-font-small-paragraph rt-font-bold rt-space-bottom05">Защита от DDoS-атак</p></a>
+                <a href=""><p class="rt-font-small-paragraph rt-font-bold rt-space-bottom05">Виртуальное хранилище</p>
+                </a>
+                <a href=""><p class="rt-font-small-paragraph rt-font-bold rt-space-bottom05">Виртуальный ЦОД</p></a>
+                <a href=""><p class="rt-font-small-paragraph rt-font-bold rt-space-bottom05">Он-лайн касса</p></a>
+                <a href=""><p class="rt-font-small-paragraph rt-font-bold rt-space-bottom05">M2M</p></a>
+              </div>
+              <div class="rt-col-4 rt-col-td-3 rt-col-md-3">
+                <a href=""><p class="rt-font-small-paragraph rt-font-bold rt-space-bottom05">Телевидение</p></a>
+                <a href=""><p class="rt-font-small-paragraph rt-font-bold rt-space-bottom05">Облачные сервисы</p></a>
+                <a href=""><p class="rt-font-small-paragraph rt-font-bold rt-space-bottom05">Видеоконференция</p></a>
+                <a href=""><p class="rt-font-small-paragraph rt-font-bold rt-space-bottom05">Аудиоконференция</p></a>
+                <a href=""><p class="rt-font-small-paragraph rt-font-bold rt-space-bottom05">Телевидение</p></a>
+              </div>
+            </div>
+
+            <h3 class="rt-font-bold rt-space-bottom2">Не нашли что искали? Свяжитесь с нами</h3>
+            <div class="rt-space-bottom5">
+              <rt-row-list :is-help-block="true" class="rt-font-left">
+                <rt-row-list-item icon="/static/images/B2B/help7.svg">
+                  <template slot="option">
+                    <p class="rt-font-paragraph rt-font-bold rt-space-bottom05">Заявкой на сайте</p>
+                    <p class="rt-font-paragraph"><a href="#">Заполните форму</a> и в ближайшее время наши специалисты перезвонят</p>
+                  </template>
+                </rt-row-list-item>
+                <rt-row-list-item icon="/static/images/B2B/help8.svg">
+                  <template slot="option">
+                    <p class="rt-font-paragraph rt-font-bold rt-space-bottom05">По телефону 8 800 200 30 00</p>
+                    <p class="rt-font-paragraph">Наши менеджеры помогут Вам  с решением проблемы</p>
+                  </template>
+                </rt-row-list-item>
+                <rt-row-list-item icon="/static/images/B2B/help9.svg">
+                  <template slot="option">
+                    <p class="rt-font-paragraph rt-font-bold rt-space-bottom05">В офисе продаж</p>
+                    <p class="rt-font-paragraph">Приходите в <a href="#">наши офисы</a>, узнавайте новости, тестируйте
+                      услуги, задавайте вопросы</p>
+                  </template>
+                </rt-row-list-item>
+              </rt-row-list>
+            </div>
+          </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+<script>
+  import componentsList from "../../componentsList";
+  export default {
+    name: "AppHelpMain",
+    components: componentsList
+  };
+</script>
