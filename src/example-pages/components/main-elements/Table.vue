8d255a9bd58efaf54fda6437cbaadc0f44454804
wikked_one <wikked616@gmail.com>
2019-04-05 12:06:23 +0300

    table without borders mobile fix

--- src/example-pages/components/main-elements/Table.vue
+++ src/example-pages/components/main-elements/Table.vue
@@ -89,85 +89,89 @@
                 </template>
               </rt-table>"
             />
-
-            <rt-table class="rt-space-bottom4">
-              <template slot="label">
-                Интернет
-              </template>
-              <template slot="columns">
-                <rt-table-col />
-                <rt-table-col width="200px" />
-              </template>
-              <template slot="header">
-                <rt-table-head-item>Услуга</rt-table-head-item>
-                <rt-table-head-item>Без опции</rt-table-head-item>
-              </template>
-              <template slot="body">
-                <rt-table-row>
-                  <rt-table-item>Все входящие звонки по России</rt-table-item>
-                  <rt-table-item>По тарифу домашней сети</rt-table-item>
-                </rt-table-row>
-              </template>
-            </rt-table>
-
-            <div class="row">
-              <div class="rt-col-6 rt-col-td-6 rt-col-md-3">
-                <rt-table :no-borders="true">
-                  <template slot="label">Разговоры без границ</template>
-                  <template slot="columns">
-                    <rt-table-col></rt-table-col>
-                    <rt-table-col width="20%"></rt-table-col>
+            <pre-code
+              text='
+                <rt-table class="rt-space-bottom4">
+                  <template slot="label">
+                    Интернет
                   </template>
-                  <template slot="description">
-                    <p class="rt-font-paragraph rt-space-bottom2">Опция «Разговоры без границ» позволяет воспользоваться льготными
-                    ценами на входящие звонки за пределами России.</p>
+                  <template slot="columns">
+                    <rt-table-col />
+                    <rt-table-col width="200px" />
+                  </template>
+                  <template slot="header">
+                    <rt-table-head-item>Услуга</rt-table-head-item>
+                    <rt-table-head-item>Без опции</rt-table-head-item>
                   </template>
                   <template slot="body">
                     <rt-table-row>
-                      <rt-table-item>Абонентская плата</rt-table-item>
-                      <rt-table-item>5 ₽/сут.</rt-table-item>
-                    </rt-table-row>
-                    <rt-table-row>
-                      <rt-table-item>Подключение</rt-table-item>
-                      <rt-table-item>0 ₽</rt-table-item>
-                    </rt-table-row>
-                    <rt-table-row>
-                      <rt-table-item>Все входящие вызовы</rt-table-item>
-                      <rt-table-item>5 ₽/мин.</rt-table-item>
+                      <rt-table-item>Все входящие звонки по России</rt-table-item>
+                      <rt-table-item>По тарифу домашней сети</rt-table-item>
                     </rt-table-row>
                   </template>
-                </rt-table>
-                <div class="d-inline-block rt-space-top3 rt-space-right4 rt-space-bottom2">
-                  <h4 class="rt-font-bold">*100*352*1#</h4>
-                  <p class="rt-font-center">подключение</p>
-                </div>
-                <div class="d-inline-block">
-                  <h4 class="rt-font-bold">*100*352*0#</h4>
-                  <p class="rt-font-center">отключение</p>
-                </div>
-                <div class="rt-col">
-                  <rt-un-wrapper unwrap-button-text="показать" :including-desktop="true" :always-visible="true" unwrapped-button-text="свернуть">
-                    <template slot="short-content">
-                      Абонентская плата взимается с момента подключения услуги до момента ее отключения абонентом.
-                      Вы можете отключить услугу по возвращению в Россию,
-                    </template>
-                    <template slot="full-content">
-                       чтобы избежать дополнительных расходов.	Тарификация услуги Мобильный Интернет в роуминге
-                      производится с интервалом 50 Кб и округлением до ближайшего целого интервала в большую сторону.
-                      Услуга действует за пределами России и доступна для подключения на всех корпоративных тарифных
-                      планах.	За сутки принимается интервал с 00:00:00 до 23:59:59 по местному времени.	Цены указаны в
-                      рублях с учетом НДС.	Список стран СНГ и Европы, в которых доступна услуга «Интернет за рубежом»
-                      Абхазия, Австрия, Азербайджан, Албания, Андорра, Армения, Белоруссия, Бельгия, Болгария, Ватикан,
-                      Великобритания, Венгрия, Германия, Грузия, Дания, Джерси и Гернси острова, Израиль, Ирландия,
-                      Исландия, Испания, Италия, Казахстан, Кипр, Кыргызстан, Латвия, Литва, Лихтенштейн, Люксембург,
-                      Мальта, Молдова, Монако, Нидерланды, Норвегия, Польша, Португалия, Румыния, Сан-Марино, Сербия,
-                      Словакия, Словения, Таджикистан, Турция, Узбекистан, Украина, Финляндия, Франция, Хорватия,
-                      Черногория, Чехия, Швейцария, Швеция, Эстония.
-                    </template>
-                  </rt-un-wrapper>
-                </div>
-              </div>
-            </div>
+                </rt-table>'
+            />
+            <pre-code
+              text='
+                <div class="row">
+                  <div class="rt-col-7 rt-col-td-3 rt-col-md-3">
+                    <rt-table :no-borders="true">
+                      <template slot="label">Опция «Везде ноль»</template>
+                      <template slot="columns">
+                        <rt-table-col></rt-table-col>
+                        <rt-table-col width="20%"></rt-table-col>
+                      </template>
+                      <template slot="description">
+                        <p class="rt-font-paragraph rt-space-bottom2">Позволяет воспользоваться льготными ценами на
+                          входящие звонки за пределами России.</p>
+                      </template>
+                      <template slot="body">
+                        <rt-table-row>
+                          <rt-table-item>Подключение</rt-table-item>
+                          <rt-table-item>0 ₽</rt-table-item>
+                        </rt-table-row>
+                        <rt-table-row>
+                          <rt-table-item>Абонентская плата</rt-table-item>
+                          <rt-table-item>100 ₽/сутки</rt-table-item>
+                        </rt-table-row>
+                        <rt-table-row>
+                          <rt-table-item>Стоимость интернет-трафика сверх включенного в абонентскую плату</rt-table-item>
+                          <rt-table-item>10 ₽/Мб.</rt-table-item>
+                        </rt-table-row>
+                      </template>
+                    </rt-table>
+                    <div class="d-inline-block rt-space-top3 rt-space-right4 rt-space-bottom2 rt-td-space-right3">
+                      <h4 class="rt-font-bold">*100*350*1#</h4>
+                      <p class="rt-font-center">подключение</p>
+                    </div>
+                    <div class="d-inline-block">
+                      <h4 class="rt-font-bold">*100*350*0#</h4>
+                      <p class="rt-font-center">отключение</p>
+                    </div>
+                    <div class="rt-col">
+                      <rt-un-wrapper unwrap-button-text="показать" :including-desktop="true" :always-visible="true" unwrapped-button-text="свернуть">
+                        <template slot="short-content">
+                          Абонентская плата взимается с момента подключения услуги до момента ее отключения абонентом.
+                          Вы можете отключить услугу по возвращению в Россию,
+                        </template>
+                        <template slot="full-content">
+                           чтобы избежать дополнительных расходов.	Тарификация услуги Мобильный Интернет в роуминге
+                          производится с интервалом 50 Кб и округлением до ближайшего целого интервала в большую сторону.
+                          Услуга действует за пределами России и доступна для подключения на всех корпоративных тарифных
+                          планах.	За сутки принимается интервал с 00:00:00 до 23:59:59 по местному времени.	Цены указаны в
+                          рублях с учетом НДС.	Список стран СНГ и Европы, в которых доступна услуга «Интернет за рубежом»
+                          Абхазия, Австрия, Азербайджан, Албания, Андорра, Армения, Белоруссия, Бельгия, Болгария, Ватикан,
+                          Великобритания, Венгрия, Германия, Грузия, Дания, Джерси и Гернси острова, Израиль, Ирландия,
+                          Исландия, Испания, Италия, Казахстан, Кипр, Кыргызстан, Латвия, Литва, Лихтенштейн, Люксембург,
+                          Мальта, Молдова, Монако, Нидерланды, Норвегия, Польша, Португалия, Румыния, Сан-Марино, Сербия,
+                          Словакия, Словения, Таджикистан, Турция, Узбекистан, Украина, Финляндия, Франция, Хорватия,
+                          Черногория, Чехия, Швейцария, Швеция, Эстония.
+                        </template>
+                      </rt-un-wrapper>
+                    </div>
+                  </div>
+                </div>'
+            />
           </div>
         </div>
       </div>
