baec0f3f94f2327d5c5acf794dc1e668f546b534
wikked_one <wikked616@gmail.com>
2019-04-16 11:06:41 +0300

    popup + tabs representation fix

--- src/lib/components/Popup/RealPopup.vue
+++ src/lib/components/Popup/RealPopup.vue
@@ -37,11 +37,21 @@
     render: function(h) {
 
       return <div class={"rtb-popup-wrapper" + ((!this.displayOnDesktop) ? " rtb-popup-wrapper--mobile-only": "")}>
-        <div class={"rtb-popup" + (this.topCenter ? " rtb-popup--top-center" : " rtb-popup--center-center")}>
-          <svg width="35" height="35" xmlns="http://www.w3.org/2000/svg" class="rtb-popup-close" viewBox="-10 -10 35 35">
-            <path d="M15 1.5L13.5 0l-6 6-6-6L0 1.5l6 6-6 6L1.5 15l6-6 6 6 1.5-1.5-6-6z" fill="#101828" fill-rule="evenodd" fill-opacity=".5"/>
-          </svg>
-          {this.$slots.default}
+        <div class="rt-container">
+          <div class="rt-col">
+            <div class="row">
+            <div class="rt-col-2 rt-col-td-d-none"/>
+            <div class="rt-col-8 rt-col-td-6 rt-col-md-3">
+              <div class={"rtb-popup" + (this.topCenter ? " rtb-popup--top-center" : " rtb-popup--center-center")}>
+                <svg width="35" height="35" xmlns="http://www.w3.org/2000/svg" class="rtb-popup-close" viewBox="-10 -10 35 35">
+                  <path d="M15 1.5L13.5 0l-6 6-6-6L0 1.5l6 6-6 6L1.5 15l6-6 6 6 1.5-1.5-6-6z" fill="#101828" fill-rule="evenodd" fill-opacity=".5"/>
+                </svg>
+                {this.$slots.default}
+              </div>
+            </div>
+            <div class="rt-col-2 rt-col-td-d-none"/>
+            </div>
+          </div>
         </div>
       </div>;
     }
