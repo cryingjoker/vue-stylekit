f1d1c131a1804a8ff3e5fc03e7f8192bbf74fe40
cryingjoker <cryingjoker@me.com>
2019-05-08 15:10:23 +0300

    fix template render error now rt-template -> tempalte replace on render + update package.json

--- src/example-pages/components/main-elements/Table.vue
+++ src/example-pages/components/main-elements/Table.vue
@@ -12,19 +12,19 @@
             <pre-code
               text="
               <rt-table class=&quot;rt-space-bottom2&quot; :tablet-list-type=&quot;true&quot;>
-                <template slot=&quot;label&quot;>
+                <rt-template slot=&quot;label&quot;>
                   Тарифы на добровольную блокировку
-                </template>
+                </rt-template>
 
-                <template slot=&quot;header&quot;>
+                <rt-template slot=&quot;header&quot;>
                   <rt-table-head-item>Территория</rt-table-head-item>
                   <rt-table-head-item>Наименование услуги</rt-table-head-item>
                   <rt-table-head-item>Бесплатный период</rt-table-head-item>
                   <rt-table-head-item>Мораторий на блокировку</rt-table-head-item>
                   <rt-table-head-item>Минимальный период блокировки</rt-table-head-item>
                   <rt-table-head-item>Стоимость добровольной блокировки в базовый период</rt-table-head-item>
-                </template>
-                <template slot=&quot;body&quot;>
+                </rt-template>
+                <rt-template slot=&quot;body&quot;>
                   <rt-table-row>
                     <rt-table-item>Архангельская и Мурманская
                       область Республика Карелия</rt-table-item>
@@ -46,26 +46,26 @@
                     <rt-table-item>30 дней</rt-table-item>
                     <rt-table-item>7 руб./в день</rt-table-item>
                   </rt-table-row>
-                </template>
+                </rt-template>
               </rt-table>
               "
             />
             <pre-code
               text="<rt-table class=&quot;rt-space-bottom2&quot;>
-                <template slot=&quot;label&quot;>
+                <rt-template slot=&quot;label&quot;>
                   Звонки
-                </template>
-                <template slot=&quot;columns&quot;>
+                </rt-template>
+                <rt-template slot=&quot;columns&quot;>
                   <rt-table-col></rt-table-col>
                   <rt-table-col width=&quot;200px&quot;></rt-table-col>
                   <rt-table-col width=&quot;200px&quot;></rt-table-col>
-                </template>
-                <template slot=&quot;header&quot;>
+                </rt-template>
+                <rt-template slot=&quot;header&quot;>
                   <rt-table-head-item>Услуга</rt-table-head-item>
                   <rt-table-head-item>С опцией</rt-table-head-item>
                   <rt-table-head-item>Без опции</rt-table-head-item>
-                </template>
-                <template slot=&quot;body&quot;>
+                </rt-template>
+                <rt-template slot=&quot;body&quot;>
                   <rt-table-row>
                     <rt-table-item>Все входящие звонки по России</rt-table-item>
                     <rt-table-item>0 руб./мин.</rt-table-item>
@@ -86,29 +86,29 @@
                     <rt-table-item :inactive=&quot;true&quot;>69 руб./мин.</rt-table-item>
                     <rt-table-item>69 руб./мин.</rt-table-item>
                   </rt-table-row>
-                </template>
+                </rt-template>
               </rt-table>"
             />
             <pre-code
               text='
                 <rt-table class="rt-space-bottom4">
-                  <template slot="label">
+                  <rt-template slot="label">
                     Интернет
-                  </template>
-                  <template slot="columns">
+                  </rt-template>
+                  <rt-template slot="columns">
                     <rt-table-col />
                     <rt-table-col width="200px" />
-                  </template>
-                  <template slot="header">
+                  </rt-template>
+                  <rt-template slot="header">
                     <rt-table-head-item>Услуга</rt-table-head-item>
                     <rt-table-head-item>Без опции</rt-table-head-item>
-                  </template>
-                  <template slot="body">
+                  </rt-template>
+                  <rt-template slot="body">
                     <rt-table-row>
                       <rt-table-item>Все входящие звонки по России</rt-table-item>
                       <rt-table-item>По тарифу домашней сети</rt-table-item>
                     </rt-table-row>
-                  </template>
+                  </rt-template>
                 </rt-table>'
             />
             <pre-code
@@ -116,16 +116,16 @@
                 <div class="row">
                   <div class="rt-col-7 rt-col-td-3 rt-col-md-3">
                     <rt-table :no-borders="true">
-                      <template slot="label">Опция «Везде ноль»</template>
-                      <template slot="columns">
+                      <rt-template slot="label">Опция «Везде ноль»</rt-template>
+                      <rt-template slot="columns">
                         <rt-table-col></rt-table-col>
                         <rt-table-col width="20%"></rt-table-col>
-                      </template>
-                      <template slot="description">
+                      </rt-template>
+                      <rt-template slot="description">
                         <p class="rt-font-paragraph rt-space-bottom2">Позволяет воспользоваться льготными ценами на
                           входящие звонки за пределами России.</p>
-                      </template>
-                      <template slot="body">
+                      </rt-template>
+                      <rt-template slot="body">
                         <rt-table-row>
                           <rt-table-item>Подключение</rt-table-item>
                           <rt-table-item>0 ₽</rt-table-item>
@@ -138,7 +138,7 @@
                           <rt-table-item>Стоимость интернет-трафика сверх включенного в абонентскую плату</rt-table-item>
                           <rt-table-item>10 ₽/Мб.</rt-table-item>
                         </rt-table-row>
-                      </template>
+                      </rt-template>
                     </rt-table>
                     <div class="d-inline-block rt-space-top3 rt-space-right4 rt-space-bottom2 rt-td-space-right3">
                       <h4 class="rt-font-bold">*100*350*1#</h4>
@@ -150,11 +150,11 @@
                     </div>
                     <div class="rt-col">
                       <rt-un-wrapper unwrap-button-text="показать" :including-desktop="true" :always-visible="true" unwrapped-button-text="свернуть">
-                        <template slot="short-content">
+                        <rt-template slot="short-content">
                           Абонентская плата взимается с момента подключения услуги до момента ее отключения абонентом.
                           Вы можете отключить услугу по возвращению в Россию,
-                        </template>
-                        <template slot="full-content">
+                        </rt-template>
+                        <rt-template slot="full-content">
                            чтобы избежать дополнительных расходов.	Тарификация услуги Мобильный Интернет в роуминге
                           производится с интервалом 50 Кб и округлением до ближайшего целого интервала в большую сторону.
                           Услуга действует за пределами России и доступна для подключения на всех корпоративных тарифных
@@ -166,7 +166,7 @@
                           Мальта, Молдова, Монако, Нидерланды, Норвегия, Польша, Португалия, Румыния, Сан-Марино, Сербия,
                           Словакия, Словения, Таджикистан, Турция, Узбекистан, Украина, Финляндия, Франция, Хорватия,
                           Черногория, Чехия, Швейцария, Швеция, Эстония.
-                        </template>
+                        </rt-template>
                       </rt-un-wrapper>
                     </div>
                   </div>
