5bd259829e912ae757980b731bcc4358cea1d697
cryingjoker <cryingjoker@me.com>
2019-08-05 09:52:54 +0300

    RTRUB2B-2694 add price float value

--- src/lib/components/Price/Price.tsx
+++ src/lib/components/Price/Price.tsx
@@ -4,164 +4,205 @@ import { Component, Prop, Watch } from "vue-property-decorator";
 @Component
 class Price extends Vue {
 
-  @Prop({ default: 0}) value: number|string;
-  @Prop({ default: 0}) oldValue: number|string;
-  @Prop({ default: "от"}) optionLabel: string;
-  @Prop({ default: null}) colorValue: string;
-  @Prop({ default: 'руб.'}) currency: string;
-  @Prop({ default: 'мес.'}) timeInterval: string;
-  @Prop({ default: false}) isOption: boolean;
-  @Prop({ default: false}) onlyPrice: boolean;
-  @Prop({ default: false}) forGame: boolean;
-  @Prop({ default: false}) boldOption: boolean;
-  @Prop({ default: false}) b2bPrice: boolean;
-  @Prop({ default: ""}) oldPriceColor: boolean;
-  @Prop({ default: false}) isTimeIntervalBottom: boolean;
+  @Prop({ default: 0 }) value: number | string;
+  @Prop({ default: 0 }) oldValue: number | string;
+  @Prop({ default: "от" }) optionLabel: string;
+  @Prop({ default: false }) showFloat: boolean;
+  @Prop({ default: null }) colorValue: string;
+  @Prop({ default: "руб." }) currency: string;
+  @Prop({ default: "мес." }) timeInterval: string;
+  @Prop({ default: false }) isOption: boolean;
+  @Prop({ default: false }) onlyPrice: boolean;
+  @Prop({ default: false }) forGame: boolean;
+  @Prop({ default: false }) boldOption: boolean;
+  @Prop({ default: false }) b2bPrice: boolean;
+  @Prop({ default: "" }) oldPriceColor: boolean;
+  @Prop({ default: false }) isTimeIntervalBottom: boolean;
 
-
-  normalizeValue: string = parseInt(this.value.toString(), 10).toString().replace(
+  //todo сделать проверку системного языка с заменой . , у double данных
+  normalizeValue: string = this.showFloat ?
+    parseFloat(this.value.toString()).toString().split('.').map((item,index)=>{
+      if(index === 0){
+        item=item.replace(
+          /(\d)(?=(\d\d\d)+([^\d]|$))/g,
+          "$1 "
+        )
+      }
+      return item
+    }).join(',') : parseInt(this.value.toString(), 10).toString().replace(
     /(\d)(?=(\d\d\d)+([^\d]|$))/g,
     "$1 "
-  );
-  normalizeOldValue: string = parseInt(this.oldValue.toString(), 10).toString().replace(
+  ).trim();
+  normalizeOldValue: string = this.showFloat ?
+      parseFloat(this.oldValue.toString()).toString().split('.').map((item,index)=>{
+        if(index === 0){
+          item=item.replace(
+            /(\d)(?=(\d\d\d)+([^\d]|$))/g,
+            "$1 "
+          )
+        }
+        return item
+      }).join(',') : parseInt(this.oldValue.toString(), 10).toString().replace(
     /(\d)(?=(\d\d\d)+([^\d]|$))/g,
     "$1 "
-  );
+  ).trim()
+
   priceType = "";
   normalizeCurrency = this.currency;
   normalizeTimeInterval = this.timeInterval;
 
-  @Watch('value', { immediate: true, deep: true })
-  onValueChanged(val: number | string, oldVal: number|string) {
-    this.normalizeValue = parseInt(val.toString(), 10).toString().replace(
+  @Watch("value", { immediate: true, deep: true })
+  onValueChanged(val: number | string, oldVal: number | string) {
+
+
+    this.normalizeValue = this.showFloat ?
+      parseFloat(val.toString()).toString().split('.').map((item,index)=>{
+        if(index === 0){
+          item=item.replace(
+            /(\d)(?=(\d\d\d)+([^\d]|$))/g,
+            "$1 "
+          )
+        }
+        return item
+      }).join(',') : parseInt(val.toString(), 10).toString().replace(
       /(\d)(?=(\d\d\d)+([^\d]|$))/g,
       "$1 "
-    );;
+      ).trim()
   }
 
-  @Watch('oldValue', { immediate: true, deep: true })
-  onOldValueChanged(val: number | string, oldVal: number|string) {
-    this.normalizeOldValue = parseInt(val.toString(), 10).toString().replace(
-      /(\d)(?=(\d\d\d)+([^\d]|$))/g,
-      "$1 "
-    );;
+  @Watch("oldValue", { immediate: true, deep: true })
+  onOldValueChanged(val: number | string, oldVal: number | string) {
+
+    this.normalizeOldValue = this.showFloat ?
+      parseFloat(val.toString()).toString().split('.').map((item,index)=>{
+        if(index === 0){
+          item=item.replace(
+            /(\d)(?=(\d\d\d)+([^\d]|$))/g,
+            "$1 "
+          )
+        }
+        return item
+      }).join(',') : parseInt(val.toString(), 10).toString().replace(
+        /(\d)(?=(\d\d\d)+([^\d]|$))/g,
+        "$1 "
+      ).trim()
   }
 
-  @Watch('currency', { immediate: true, deep: true })
+  @Watch("currency", { immediate: true, deep: true })
   onCurrencyChanged(val: string, oldVal: string) {
     this.normalizeCurrency = val;
   }
 
-  @Watch('timeInterval', { immediate: true, deep: true })
+  @Watch("timeInterval", { immediate: true, deep: true })
   onTimeIntervalChanged(val: string, oldVal: string) {
     this.normalizeTimeInterval = val;
   }
 
-  mounted(){
+  mounted() {
 
   }
 
   render(h: CreateElement): VNode {
-    const rtPriceClass = 'rt-price'+(this.forGame ? ' rt-price-game' : '');
-    const rtPriceInfoClass = 'rt-price__info'+(this.boldOption ? ' rt-price__info--bold-font' : '');
-    const timeIntervalRender = ()=>{
-      if(this.normalizeTimeInterval && !this.onlyPrice) {
+    const rtPriceClass = "rt-price" + (this.forGame ? " rt-price-game" : "");
+    const rtPriceInfoClass = "rt-price__info" + (this.boldOption ? " rt-price__info--bold-font" : "");
+    const timeIntervalRender = () => {
+      if (this.normalizeTimeInterval && !this.onlyPrice) {
         {
           return <div class="rt-price__info-item">
             {this.normalizeTimeInterval}
-          </div>
+          </div>;
         }
-      }else{
-        return null
+      } else {
+        return null;
       }
     };
-    const opinionRender = ()=>{
-      if( this.isOption){
-        return <div class="rt-price__opinion">{this.optionLabel}</div>
-      }else{
-        return null
+    const opinionRender = () => {
+      if (this.isOption) {
+        return <div class="rt-price__opinion">{this.optionLabel}</div>;
+      } else {
+        return null;
       }
     };
-    const currencyRender = ()=>{
-      if(this.normalizeCurrency) {
+    const currencyRender = () => {
+      if (this.normalizeCurrency) {
         {
           return <div class="rt-price__info-item">
             {this.normalizeCurrency}
-          </div>
+          </div>;
         }
-      }else{
-        return null
+      } else {
+        return null;
       }
-    }
-    const oldValueRender = ()=>{
-      if(this.normalizeOldValue && parseInt(this.normalizeOldValue) > 0) {
-        return <div class="rt-price__old-value">{this.normalizeOldValue}</div>
-      }else{
-        return null
+    };
+    const oldValueRender = () => {
+      if (this.normalizeOldValue && parseFloat(this.normalizeOldValue) > 0) {
+        return <div class="rt-price__old-value">{this.normalizeOldValue}</div>;
+      } else {
+        return null;
       }
-    }
-    const priceInfoRender = ()=>{
-      if(this.isTimeIntervalBottom) {
-        return  <div>
-                  {opinionRender()}
-                  <div class={"rt-price__value rtb-price__value" + (this.colorValue ? ' color-' + this.colorValue : '')}>
-                    {this.normalizeValue}
-                  </div>
-                  <div class="rtb-price__info rt-price__info">{this.normalizeCurrency}</div>
-                  <div class="rt-font-small-paragraph rt-font-bold rtb-price__info-interval">{this.normalizeTimeInterval}</div>
-                </div>
-      }else{
+    };
+    const priceInfoRender = () => {
+      if (this.isTimeIntervalBottom) {
         return <div>
-                  {opinionRender()}
-                  <div class={"rt-price__value rtb-price__value" + (this.colorValue ? ' color-' + this.colorValue : '')}>
-                    {this.normalizeValue}
-                  </div>
-                  <div class="rt-price__info">
-                    <div class="rt-price__info rtb-price__info">
-                      <div class="rt-price__info-item rtb-price__info-item">{this.normalizeCurrency}</div>
-                      <div class="rtb-price__info-item rtb-price__info-item">{this.normalizeTimeInterval}</div>
-                    </div>
-                  </div>
-                </div>
-      }
-    }
-    if(this.b2bPrice) {
-      const oldB2bPriceRender = ()=>{
-        if(this.normalizeOldValue && parseInt(this.normalizeOldValue) > 0) {
-          return <div class="rtb-price__old-value">
-            <div
-              class={"rt-price__old-value rtb-price__old-value" + (this.oldPriceColor ? ' color-' + this.oldPriceColor : '')}>{this.normalizeOldValue}
-              <div class={"rt-price__info rtb-price__info-item"}>
-              <span
-                class={(this.oldPriceColor ? ' color-' + this.oldPriceColor : '')}>{this.normalizeCurrency}</span>
-              </div>
+          {opinionRender()}
+          <div class={"rt-price__value rtb-price__value" + (this.colorValue ? " color-" + this.colorValue : "")}>
+            {this.normalizeValue}
+          </div>
+          <div class="rtb-price__info rt-price__info">{this.normalizeCurrency}</div>
+          <div class="rt-font-small-paragraph rt-font-bold rtb-price__info-interval">{this.normalizeTimeInterval}</div>
+        </div>;
+      } else {
+        return <div>
+          {opinionRender()}
+          <div class={"rt-price__value rtb-price__value" + (this.colorValue ? " color-" + this.colorValue : "")}>
+            {this.normalizeValue}
+          </div>
+          <div class="rt-price__info">
+            <div class="rt-price__info rtb-price__info">
+              <div class="rt-price__info-item rtb-price__info-item">{this.normalizeCurrency}</div>
+              <div class="rtb-price__info-item rtb-price__info-item">{this.normalizeTimeInterval}</div>
             </div>
           </div>
-        }else{
-          return null
-        }
+        </div>;
       }
+    };
+    if (this.b2bPrice) {
+      const oldB2bPriceRender = () => {
+        if (this.normalizeOldValue && parseFloat(this.normalizeOldValue) > 0) {
+          return <div class="rtb-price__old-value">
+            <div
+              class={"rt-price__old-value rtb-price__old-value" + (this.oldPriceColor ? " color-" + this.oldPriceColor : "")}>{this.normalizeOldValue}
+              <div class={"rt-price__info rtb-price__info-item"}>
+              <span
+                class={(this.oldPriceColor ? " color-" + this.oldPriceColor : "")}>{this.normalizeCurrency}</span>
+              </div>
+            </div>
+          </div>;
+        } else {
+          return null;
+        }
+      };
       return <div class="rt-price rtb-price rt-price-without-space">
-          {oldB2bPriceRender()}
-          {priceInfoRender()}
-        </div>
+        {oldB2bPriceRender()}
+        {priceInfoRender()}
+      </div>;
     } else {
       return <div class={`${rtPriceClass} rt-price-without-space`}>
         {opinionRender()}
         {oldValueRender()}
         <div
-          class={"rt-price__option-value rt-price__value" + (this.colorValue ? ' color-' + this.colorValue : '')}>{this.normalizeValue}</div>
+          class={"rt-price__option-value rt-price__value" + (this.colorValue ? " color-" + this.colorValue : "")}>{this.normalizeValue}</div>
         <div class={rtPriceInfoClass}>
           {currencyRender()}
           {timeIntervalRender()}
         </div>
-      </div>
+      </div>;
     }
   }
 
 
-
 }
+
 export default {
   component: Price,
   name: "RtPrice"
