84aaad77992102ba5f66bfa99b8ceb63bb4fa66e
cryingjoker <cryingjoker@me.com>
2019-10-24 10:58:56 +0300

    add example for fullscreen image

--- src/example-pages/components/main-elements/FullscreenImage.vue
+++ src/example-pages/components/main-elements/FullscreenImage.vue
@@ -38,7 +38,7 @@
     <pre-code
       text='<rt-fullscreen-image
       background-image="https://img00.deviantart.net/3949/i/2018/277/7/a/s_f_c_city_by_renata_s_art-dcoj7ap.png"
-    >'></pre-code>
+    >
       <div class="rt-container">
         <div class="rt-col-12 rt-col-md-3">
           <p class="rt-space-top">
@@ -65,7 +65,7 @@
           </p>
         </div>
       </div>
-    </rt-fullscreen-image>
+    </rt-fullscreen-image>'></pre-code>
     <div class="app-body">
       <div class="rt-container">
         <div class="rt-col-12 rt-col-md-3">
