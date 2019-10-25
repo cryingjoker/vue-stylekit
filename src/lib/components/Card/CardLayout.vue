23b740b41f043036798a81e448c1bbddb4939f76
UF\Mikhail.Malyuga <Mikhail.Malyuga@south.rt.ru>
2019-10-25 18:27:08 +0300

    2987 Правки для IE 1. childNodes - псевдомассив, forEach в нем нет. Переписал на for 2. метода node.remove нет. Заменил на parentNode.removeChild 3. для overflow нет значения unset. Заменил на visible

--- src/lib/components/Card/CardLayout.vue
+++ src/lib/components/Card/CardLayout.vue
@@ -53,21 +53,24 @@
       },
       specifyCardClass() {
         if(this.$slots.cards && window.innerWidth <= parseInt(variables["tablet-upper-limit"])){
-          this.$el.childNodes[0].childNodes[2].childNodes[0].childNodes.forEach(el => {
+          for (let i = 0; i < this.$el.childNodes[0].childNodes[2].childNodes[0].childNodes.length; i++) {
+            let el = this.$el.childNodes[0].childNodes[2].childNodes[0].childNodes[i];
             el.classList !== undefined && el.classList.contains('rt-col-md-3') ? el.classList.add('rtk-carousel-slide') : null;
-          });
+          }
           this.singleRow ? this.$el.classList.add('row') : null;
           this.singleRow ? this.$el.parentNode.classList.add('rt-col') : null;
         } else if(this.$slots.cards && window.innerWidth <= parseInt(variables["laptop-upper-limit"]) && window.innerWidth >= parseInt(variables["laptop-lower-limit"])) {
-          this.$el.childNodes[0].childNodes[0].childNodes[0].childNodes.forEach(el => {
+          for (let i = 0; i < this.$el.childNodes[0].childNodes[0].childNodes[0].childNodes.length; i++) {
+            let el = this.$el.childNodes[0].childNodes[0].childNodes[0].childNodes[i];
             el.classList !== undefined && el.classList.contains('rt-col-md-3') ? el.classList.add('rtk-carousel-slide') : null;
-          });
+          }
 //          this.singleRow ? this.$el.classList.add('row') : null;
 //          this.singleRow ? this.$el.parentNode.classList.remove('rt-col') : null;
         } else if(this.$slots.cards && window.innerWidth > parseInt(variables["desktop-lower-limit"])) {
-          this.$el.childNodes[0].childNodes[0].childNodes[0].childNodes.forEach(el => {
+          for (let i = 0; i < this.$el.childNodes[0].childNodes[0].childNodes[0].childNodes.length; i++) {
+            let el = this.$el.childNodes[0].childNodes[0].childNodes[0].childNodes[i];
             el.classList !== undefined && el.classList.contains('rtk-carousel-slide') ? el.classList.remove('rtk-carousel-slide') : null;
-          });
+          };
           this.singleRow ? this.$el.classList.remove('row'): null;
           this.singleRow ? this.$el.parentNode.classList.remove('rt-col') : null;
         }
