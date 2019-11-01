adf517dd77575776ac681374a97adb0a9012fe9a
Бочкарев Владимир Геннадьевич <vladimir.bochkarev@south.rt.ru>
2019-11-01 09:46:21 +0300

    Input.vue Error: Avoid mutating a prop directly. FIXED!

--- src/lib/components/Input/Input.vue
+++ src/lib/components/Input/Input.vue
@@ -1,10 +1,15 @@
 <script type="text/jsx">
+  /* eslint-disable vue/return-in-computed-property */
   import Vue from "vue";
   import VeeValidate from "vee-validate";
   import variables from "../../variables.json";
 
   export default {
     name: "RtInput",
+    model: {
+      prop: 'value',
+      event: 'input'
+    },
     props: {
       customRules: {
         type: Array,
@@ -66,7 +71,10 @@
         type: String,
         default: null
       },
-      validate: {},
+      validate: {
+        type: Object,
+        default: null
+      },
       showNumbersButtons: {
         type: Boolean,
         default: false
@@ -130,7 +138,8 @@
         localLabel: this.label,
         localValue: this.value ? this.value : "",
         hasInputText: this.value ? this.value.length > 0 : false,
-        hintPosition: "right"
+        hintPosition: "right",
+        passwordVisibilityLocal: this.passwordVisibility
       };
     },
 
@@ -155,7 +164,7 @@
           className += " rtb-input--hidden";
         }
         if (this.approved) {
-          className += " rtb-input--approved"
+          className += " rtb-input--approved";
         }
         return className;
       }
@@ -199,18 +208,18 @@
             const that = this;
             that["_events"][eventName].forEach((fn)=> {
               if(eventName != 'input' && window[variables.globalSettingsKey].segment != 'b2c') { // for work with v-model
-                this.$refs.input.addEventListener(eventName, fn)
+                this.$refs.input.addEventListener(eventName, fn);
               } else {
                 this.$refs.input.addEventListener(
                   eventName,
                   function() {
                     if (that["_events"] && that["_events"][eventName] && that["_events"][eventName][0] && typeof that["_events"][eventName][0] === 'function') {
-                      that["_events"][eventName][0](arguments[0])
+                      that["_events"][eventName][0](arguments[0]);
                     }
                   }
                 );
               }
-            })
+            });
           });
         }
       },
@@ -374,7 +383,7 @@
         } else if (inputElement.getAttribute("type") === "text") {
           inputElement.setAttribute("type", "password");
         }
-        this.passwordVisibility = !this.passwordVisibility;
+        this.passwordVisibilityLocal = !this.passwordVisibilityLocal;
       },
       mask(e) {
         let matrix = "+7 (___) ___ ____",
@@ -471,7 +480,7 @@
 
       const passwordIcon = (() => {
         if (this.type === "password") {
-          if (!this.passwordVisibility) {
+          if (!this.passwordVisibilityLocal) {
             return <div class="password-icon password-hidden" onClick={this.togglePasswordVisibility}>
               <svg width="20" height="10" xmlns="http://www.w3.org/2000/svg" class="password-icon__icon">
                 <g stroke="#575D68" stroke-width="2" fill="none" fill-rule="evenodd" stroke-linecap="round">
@@ -562,7 +571,7 @@
             name={this.fieldName}
             onInput={this.inputHandler}
             v-validate={this.validate}
-          />
+          />;
         } else {
           return <input
             onKeypress={this.keyPress}
@@ -577,7 +586,7 @@
             v-validate={this.validate}
             onFocus={this.mask}
             onBlur={this.mask}
-          />
+          />;
         }
       })();
 
@@ -599,4 +608,3 @@
     }
   };
 </script>
-
