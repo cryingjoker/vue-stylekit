a45cafe0dacfaa5e77ec31effc36ef99958e59eb
Сидорец Виктор Алексеевич <Viktor.Sidorets@UF.RT.RU>
2020-01-15 17:25:00 +0300

    RTRU-6031 - Полный набор иконок

---
+++ static/icons/rn__TV_ONLINE_STAR-C80px.js
@@ -0,0 +1,1 @@
+[{'rn__TV_ONLINE_STAR-C80px': {width: 80,height: 80,viewBox: '0 0 80 80',data: '<g _fill="none" fill-rule="evenodd"><circle pid="0" _fill="#101828" cx="40" cy="40" r="40"/><path pid="1" _fill="#FF4F12" fill-rule="nonzero" d="M27.75 32.265l9.135-8.015L52.25 40 36.885 55.75l-9.135-8.015L35.66 40z"/><path pid="2" d="M12 12h56v56H12z"/></g>'}}]
